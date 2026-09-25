2a59b854374f49b79f52d49c9f6f2df465a3ac93
aleexds <cuberoduartealex@gmail.com>
2026-09-25 11:19:08 -0600

    loginEnRouting

--- reactInicio/src/routes/Routing.jsx
+++ reactInicio/src/routes/Routing.jsx
@@ -1,12 +1,16 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import Login from "../pages/AuthView"
 
 export default function Routing() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/LOGIN" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
